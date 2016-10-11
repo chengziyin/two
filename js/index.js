86df80e895a98c3f3020eab9c7881338746fb4e4
chengziyin <1816821129@qq.com>
2016-10-11 23:13:24 +0800

    login
    add login

--- js/index.js
+++ js/index.js
@@ -14,6 +14,7 @@ var student = [
 ]
 
 
+
 for (var i = 0; i < student.length; i ++){
 	var n = info.rowIndex + 1;
 	var tr = info.insertRow(n);
@@ -29,6 +30,7 @@ for (var i = 0; i < student.length; i ++){
 // var td = tr.insertCell(message[i]);
 // td.innerHTML = Math.random();
 // }
+
 var m = document.getElementsByTagName('input');
 var addBt = document.getElementById('addBtn');
 
@@ -61,5 +63,172 @@ function show(){
 		}
 	}	
 }
+
+
+var logIn = document.getElementById('logInBtn');
+var signIn = document.getElementById('signInBtn');
+var showD = document.getElementById('showDiv');
+var logInPal = document.createElement('div');
+var submitBtn = document.getElementById('logIn');
+
+var old = submitBtn;
+var oldInner = submitBtn.innerHTML;
+//登陆按钮点击事件
+logIn.onclick = function(){
+	showD.style.display = 'block';
+	showDiv();
+	if(logInPal.innerHTML != null){
+		logInPal.innerHTML = null;
+		logInDiv();
+	}else{
+	return logInPal.innerHTML;
+	}
+}
+
+//注册按钮点击事件
+signIn.onclick = function(){
+	showD.style.display = 'block';
+	showDiv();
+}
+
+//显示中间div
+function showDiv(){
+	logInPal.style.width = '400px';
+	logInPal.style.height = '280px';
+	logInPal.style.position = 'relative';
+	logInPal.style.top = '100px';
+	logInPal.style.left = '50%';
+	logInPal.style.marginLeft = '-200px';
+	logInPal.style.backgroundColor = '#fff';
+	logInPal.style.zIndex = '8888';
+	var object = showD.appendChild(logInPal);
+}
+
+//隐藏弹出框
+function hideDiv(){
+	showD.style.display = 'none';
+}
+
+//显示登陆框内容
+function logInDiv(){	
+	var logInItem = document.createElement('div');
+	var object = logInPal.appendChild(logInItem);
+	logInItem.style.height = '50px';
+	logInItem.style.marginTop = '20px';
+	logInItem.innerHTML = '请输入用户名，密码登陆';
+	logInItem.style.textAlign = 'center';
+	logInItem.style.fontSize = '30px';
+	logInItem.style.paddingTop = '13px';
+	logInItem.style.backgroundColor = 'rgba(126,233,108,1)';
+	var name = document.createElement('input');
+	var passWord = document.createElement('input');
+	name.type = 'text';
+	name.style.display = 'block';
+	name.style.height = '35px';
+	name.style.width = '300px';
+	name.style.margin = '10px auto';
+	name.style.fontSize = '24px';
+	name.style.borderRadius = '6px';
+	name.placeholder = '请输入用户名/邮箱/手机号';
+	var nameList = name.value;
+
+	passWord.type = 'passWord';
+	passWord.style.display = 'block';
+	passWord.style.height = '35px';
+	passWord.style.width = '300px';
+	passWord.style.margin = '10px auto';
+	passWord.style.fontSize = '24px';
+	passWord.style.borderRadius = '6px';
+	passWord.placeholder = '请输入密码';
+	var passWordList = passWord.value;
+	
+	var btnLogIn = document.createElement('button');
+	btnLogIn.style.display = 'block';
+	btnLogIn.style.margin = '20px auto';
+	btnLogIn.innerHTML = '登陆';
+	btnLogIn.style.height = '35px';
+	btnLogIn.style.width = '300px';
+	btnLogIn.style.backgroundColor = 'green';
+	btnLogIn.style.fontSize = '22px';
+	btnLogIn.style.textAlign = 'center';
+	btnLogIn.style.color = '#fff';
+	btnLogIn.style.cursor = 'pointer';
+
+	var txt = document.createElement('a');
+	txt.innerHTML = '切换到快速登陆';
+	txt.setAttribute('Id','change');
+	txt.href = "#";
+	txt.style.textDecoration = 'none';
+	txt.style.color = '#f00';
+	txt.style.float = 'right';
+	txt.style.marginRight = '50px';
+
+	
+	var object = logInPal.appendChild(name);
+	var object = logInPal.appendChild(passWord);
+	var object = logInPal.appendChild(btnLogIn);
+	var object = logInPal.appendChild(txt);
+
+	var aBtn = document.getElementById('change');
+
+	var person = [
+		['肖明声','12345678']
+	]
+
+
+
+	aBtn.onclick = function(){
+		if(txt.innerHTML == '切换到快速登陆'){
+			for(var i = 0; i < person.length; i ++){
+				for(var j = 0; j < person[i].length; j ++){
+					name.value = person[i][0];
+					passWord.value = person[i][1];
+					// alert(person[i][j]);
+				}
+			}
+		txt.innerHTML = '切换回普通模式';
+		}else if(txt.innerHTML == '切换回普通模式'){
+			txt.innerHTML = '切换到快速登陆';
+			name.value = '';
+			passWord.value = '';
+		}
+	}
+
+
+	btnLogIn.onclick = function(){
+
+		if(name.value != '' && passWord.value != ''){
+			hideDiv();
+			submitBtn.innerHTML = '用户名:' + name.value;
+		}else if(name.value == ''){
+				name.value = '请输入用户名';
+		}else if(passWord.value == ''){
+				alert('请输入密码');
+		}
+	}
+}
+
+var logOut = document.createElement('button');
+submitBtn.onmouseover = function(){
+	if(submitBtn.innerHTML.match('用户名') && !submitBtn.innerHTML.match('退出')){	
+	logOut.innerHTML = '退出';
+	// logOut.style.position = 'relative';
+	// logOut.style.top = '20px';
+	// logOut.style.width = '200px';
+	logOut.style.height = '30px';
+	logOut.style.fontSize = '22px';
+	logOut.style.cursor = 'pointer';
+	var object = submitBtn.appendChild(logOut);
+	}
+}
+
+logOut.onclick = function(){
+		submitBtn.innerHTML = oldInner;
+		submitBtn = old;
+	}
+
+// submitBtn.onmouseout = function(){
+// 	logOut.style.display = 'none';
+// }
 
 
